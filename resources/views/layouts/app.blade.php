6079df578dd07785f955559760fc94bebb348a6a
devprinceng <devprinceng@gmail.com>
2024-02-14 06:07:28 +0100

    fixed image bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,7 +114,8 @@
                         <div class="ftco-footer-widget mb-4">
                             <h2 class="ftco-heading-2">Recent Blog</h2>
                             <div class="block-21 mb-4 d-flex">
-                                <a class="blog-img mr-4" style="background-image: url(images/image_1.jpg);"></a>
+                                <a class="blog-img mr-4"
+                                    style="background-image: url({{ asset('assets/images/image_1.jpg') }});"></a>
                                 <div class="text">
                                     <h3 class="heading"><a href="#">Even the all-powerful Pointing has no
                                             control about</a></h3>
@@ -127,7 +128,8 @@
                                 </div>
                             </div>
                             <div class="block-21 mb-4 d-flex">
-                                <a class="blog-img mr-4" style="background-image: url(images/image_2.jpg);"></a>
+                                <a class="blog-img mr-4"
+                                    style="background-image: url({{ asset('assets/images/image_2.jpg') }});"></a>
                                 <div class="text">
                                     <h3 class="heading"><a href="#">Even the all-powerful Pointing has no
                                             control about</a></h3>
