2a4223639b0d9ac5d35facd79c74ba1f84157ead
devprinceng <devprinceng@gmail.com>
2024-02-14 06:09:04 +0100

    added single product route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('products/product-{id}', [App\Http\Controllers\Products\ProductsController::class, 'viewproduct'])->name('product.view');
